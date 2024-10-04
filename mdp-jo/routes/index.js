6700f14c1e3e0c1ac1b895de7ae06112d59897c0
Jovan-Y <yometmank@gmail.com>
2024-10-04 10:59:31 +0700

    asd
    asd

--- mdp-jo/routes/index.js
+++ mdp-jo/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+   const berita= [
+        {
+            judul: "berita 1",
+            isi : "isi berita 1"
+        },
+        {
+            judul: "berita 2",
+            isi : "isi berita 2"
+        },
+    ];
+    res.render('index', {title: 'halaman home',berita, layout: 'main'});
 });
 
 module.exports = router;
